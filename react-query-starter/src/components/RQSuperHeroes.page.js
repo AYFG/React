55645c2253fad5f4fee8191e8ec4739fbd91befe
AYFG <sek82468246@gmail.com>
2024-01-08 17:11:31 +0900

    mutate를 이용한 post

--- react-query-starter/src/components/RQSuperHeroes.page.js
+++ react-query-starter/src/components/RQSuperHeroes.page.js
@@ -1,19 +1,41 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useSuperHeroesData } from "./hooks/useSuperHeroesData";
+import { useAddSuperHeroData } from "./hooks/useSuperHeroesData";
 
 export const RQSuperHeroesPage = () => {
+  const [name, setName] = useState("");
+  const [alterEgo, setAlterEgo] = useState("");
+
   const onSuccess = (data) => {
     console.log("데이터 가져오기 후 콜백 함수 실행", data);
   };
   const onError = (error) => {
     console.log("오류 발생 후 콜백 함수 실행", error);
   };
-  const { isLoading, data, isError, error, isFetching } = useSuperHeroesData(
-    onSuccess,
-    onError
-  );
+  const { isLoading, data, isError, error, isFetching, refetch } =
+    useSuperHeroesData(onSuccess, onError);
 
-  console.log({ isLoading, isFetching });
+  const {
+    mutate: addHero,
+    isLoading: mutateLoading,
+    isError: mutateError,
+    error: mutateErrorMessage,
+  } = useAddSuperHeroData();
+
+  if (mutateLoading) {
+    return <h2>Post Loading...</h2>;
+  }
+  if (mutateError) {
+    return <h2>{mutateErrorMessage.message}</h2>;
+  }
+
+  const handleAddHeroClick = () => {
+    console.log({ name, alterEgo });
+    const hero = { name, alterEgo };
+    addHero(hero);
+  };
+
   if (isLoading || isFetching) {
     return <h2>Loading...</h2>;
   }
@@ -23,6 +45,20 @@ export const RQSuperHeroesPage = () => {
   return (
     <>
       <h2>React Query Super Heroes Page</h2>
+      <div>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          value={alterEgo}
+          onChange={(e) => setAlterEgo(e.target.value)}
+        />
+        <button onClick={handleAddHeroClick}>Add Hero</button>
+      </div>
+      <button onClick={refetch}>Fetch Heroes</button>
       {data?.data.map((hero) => {
         return (
           <div key={hero.id}>
