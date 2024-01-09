8c71a3402eea118412579e98c22fa1aaf1c12193
AYFG <sek82468246@gmail.com>
2024-01-10 02:52:36 +0900

    119 page Counter Class Components

--- reactwithts/class-counter/src/App.tsx
+++ reactwithts/class-counter/src/App.tsx
@@ -1,24 +1,61 @@
-import logo from "./logo.svg";
-import "./App.css";
+import styled from '@emotion/styled';
+import { useState } from 'react';
+import { Button } from './components/Button/index';
+import { Label } from './components/Label/index';
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+const Title = styled.h1`
+  margin-bottom: 32px;
+`;
+const Contents = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+// const Label = styled.span`
+//   margin: 0 16px;
+//   font-size: 1.2rem;
+// `;
+// const Button = styled.button`
+//   border: 0;
+//   color: #ffffff;
+//   background-color: #ff5722;
+//   cursor: pointer;
+//   padding: 8px 16px;
+//   border-radius: 4px;
+//   &:hover {
+//     background-color: #ff5722;
+//     opacity: 0.8;
+//   }
+//   &:active {
+//     box-shadow: inset 5px 5px 10px rgba(0, 0, 0, 0.2);
+//   }
+// `;
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const sub = () => {
+    setCounter(counter - 1);
+  };
+  const add = () => {
+    setCounter(counter + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Title>Counter App</Title>
+      <Contents>
+        <Button label="-" onClick={sub}></Button>
+        {/* <Label>{counter}</Label> */}
+        <Label data={counter} />
+        <Button label="+" onClick={add}></Button>
+      </Contents>
+    </Container>
   );
 }
 
