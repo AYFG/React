400634fd3a6699db5aa003efdb9bc42a7c7b3ef3
AYFG <sek3137@naver.com>
2024-02-25 21:29:34 +0900

    Link Component Navigation

--- Next.js/routing-demo/src/app/page.tsx
+++ Next.js/routing-demo/src/app/page.tsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <p>Welcome Home!</p>;
+  return (
+    <>
+      <p>Welcome Home!</p>
+      <Link href="/blog">Blog</Link>
+      <Link href="/products">Products</Link>
+    </>
+  );
 }
