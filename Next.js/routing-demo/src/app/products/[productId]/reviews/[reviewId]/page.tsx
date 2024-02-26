7c4b0e4033e74dffbbf42efda5da8f548b1e36eb
AYFG <sek82468246@gmail.com>
2024-02-26 23:53:45 +0900

    error.tsx로 에러 관리

--- Next.js/routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ Next.js/routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,5 +1,9 @@
 import { notFound } from "next/navigation";
 
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
+
 export default function ReviewDetail({
   params,
 }: {
@@ -8,6 +12,10 @@ export default function ReviewDetail({
     reviewId: string;
   };
 }) {
+  const random = getRandomInt(2);
+  if (random === 1) {
+    throw new Error("난수를 이용한 에러 핸들링 학습중");
+  }
   if (parseInt(params.reviewId) > 1000) {
     notFound();
   }
