cd6b5ba8cd35e0d27bdef21a7994ef5db239cbc9
AYFG <sek82468246@gmail.com>
2024-02-28 15:30:35 +0900

    Parallel Routes

---
+++ Next.js/routing-demo/src/app/complex-dashboard/page.tsx
@@ -0,0 +1,10 @@
+export default function ComplexDashBoard() {
+  return <div>ComplexDashBoard</div>;
+}
+// Next.js의 parallel routes는 슬롯으로 알려진 기능을 사용하여 정의됩니다
+
+// 슬롯은 모듈식으로 콘텐츠를 구성하는 데 도움이 됩니다
+
+// 슬롯을 정의하기 위해 "@folder" 명명 규칙을 사용합니다
+
+// 그런 다음 각 슬롯은 해당 "layout.tsx" 파일에 소품으로 전달됩니다.
