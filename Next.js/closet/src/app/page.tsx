3f6c4b1fa4108bdda86a5676d9608a1b5b6a704d
AYFG <sek82468246@gmail.com>
2024-02-28 20:48:27 +0900

    closet login

---
+++ Next.js/closet/src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <>
+      <div className="">
+        <img src={`/mainImg.png`} className="h-800" />
+      </div>
+      <div className="text-center">
+        <h1 className="mt-20">Weekly Best</h1>
+      </div>
+    </>
+  );
+}
