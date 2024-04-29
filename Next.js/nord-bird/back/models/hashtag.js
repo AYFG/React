6f9eba0a867434ab9eeac42dd3be5f8c5d661942
AYFG <sek82468246@gmail.com>
2024-04-29 16:34:34 +0900

    prepare for aws

--- Next.js/nord-bird/back/models/hashtag.js
+++ Next.js/nord-bird/back/models/hashtag.js
@@ -1,20 +1,23 @@
-module.exports = (sequelize, DataTypes) => {
-  const Hashtag = sequelize.define(
-    "Hashtag",
-    {
-      // id는 기본적으로 들어있다.
-      name: {
+const DataTypes = require("sequelize");
+const { Model } = DataTypes;
+
+module.exports = class Hashtag extends Model {
+  static init(sequelize) {
+    return super.init(
+      {
         type: DataTypes.STRING(20),
         allowNull: false,
       },
-    },
-    {
-      charset: "utf8mb4",
-      collate: "utf8mb4_general_ci", //한글,이모티콘(mb4) 저장
-    }
-  );
-  Hashtag.associate = (db) => {
+      {
+        modelName: "Hashtag",
+        tableName: "hashtags",
+        charset: "utf8mb4",
+        collate: "utf8mb4_general_ci", //한글,이모티콘(mb4) 저장
+        sequelize,
+      }
+    );
+  }
+  static associate(db) {
     db.Hashtag.belongsToMany(db.Post, { through: "PostHashtag" });
-  };
-  return Hashtag;
+  }
 };
--- Next.js/nord-bird/back/models/post.js
+++ Next.js/nord-bird/back/models/post.js
@@ -1,26 +1,30 @@
-module.exports = (sequelize, DataTypes) => {
-  const Post = sequelize.define(
-    "Post",
-    {
-      // id는 기본적으로 들어있다.
-      content: {
-        type: DataTypes.TEXT,
-        allowNull: false,
+const DataTypes = require("sequelize");
+const { Model } = DataTypes;
+
+module.exports = class Post extends Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        content: {
+          type: DataTypes.TEXT,
+          allowNull: false,
+        },
       },
-    },
-    {
-      charset: "utf8mb4",
-      collate: "utf8mb4_general_ci", //한글,이모티콘(mb4) 저장
-    }
-  );
-  // add,get,set,remove
-  Post.associate = (db) => {
+      {
+        modelName: "Post",
+        tableName: "posts",
+        charset: "utf8mb4",
+        collate: "utf8mb4_general_ci",
+        sequelize,
+      }
+    );
+  }
+  static associate(db) {
     db.Post.belongsTo(db.User); //post.addUser , post.getUser , post.setUser
     db.Post.belongsToMany(db.Hashtag, { through: "PostHashtag" }); //post.addHashtags
     db.Post.hasMany(db.Comment); //post.addComments , post.getComments
     db.Post.hasMany(db.Image); // post.addImages , post.getImages
     db.Post.belongsToMany(db.User, { through: "Like", as: "Likers" }); // post.addLikers , post.removeLikes
     db.Post.belongsTo(db.Post, { as: "Retweet" }); // post.addRetweet
-  };
-  return Post;
+  }
 };
