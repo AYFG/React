7bb5931a53e6f2e31605a1ea4fa5c827bf217975
AYFG <sek3137@naver.com>
2025-01-13 15:47:14 +0900

    delete, delete press style=  android_ripple | 조건부 스타일

--- React-Native/RNCourse/app/(tabs)/laboratory.tsx
+++ React-Native/RNCourse/app/(tabs)/laboratory.tsx
@@ -21,6 +21,12 @@ export default function laboratory() {
       { text: enteredGoalText, id: Math.random().toString() },
     ]);
   }
+
+  function deleteGoalHandler(id: string) {
+    setCourseGoals((currentCourseGoals) => {
+      return currentCourseGoals.filter((goal) => goal.id !== id);
+    });
+  }
   return (
     <View style={styles.appContainer}>
       <GoalInput onAddGoal={addGoalHandler} />
@@ -28,7 +34,13 @@ export default function laboratory() {
         <FlatList
           data={courseGoals}
           renderItem={(itemData) => {
-            return <GoalItem text={itemData.item.text} />;
+            return (
+              <GoalItem
+                text={itemData.item.text}
+                id={itemData.item.id}
+                onDeleteItem={deleteGoalHandler}
+              />
+            );
           }}
           keyExtractor={(item, index) => {
             return item.id;
--- React-Native/RNCourse/components/GoalItem.tsx
+++ React-Native/RNCourse/components/GoalItem.tsx
@@ -1,9 +1,25 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
-export default function GoalItem(props: { text: string }) {
+export default function GoalItem(
+  this: string,
+  props: {
+    text: string;
+    id: string;
+    onDeleteItem: (id: string) => void;
+  },
+) {
+  // function deleteItemHandler(){
+  //   props.onDeleteItem(id:string );
+  // }
   return (
     <View style={styles.goalItem}>
-      <Text style={styles.goalText}>{props.text}</Text>
+      <Pressable
+        android_ripple={{ color: "#210644" }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
+        <Text style={styles.goalText}>{props.text}</Text>
+      </Pressable>
     </View>
   );
 }
@@ -11,11 +27,14 @@ export default function GoalItem(props: { text: string }) {
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: "#5e0acc",
   },
   goalText: {
+    padding: 8,
     color: "white",
+  },
+  pressedItem: {
+    opacity: 0.6,
   },
 });
