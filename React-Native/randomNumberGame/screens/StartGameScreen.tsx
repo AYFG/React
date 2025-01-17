5f7e0e9d4fc128b7be00fdb22ebb51056c007be5
AYFG <sek82468246@gmail.com>
2025-01-17 19:38:42 +0900

    textInput 글자 수 제한, 키보드 타입 숫자 패드

--- React-Native/randomNumberGame/screens/StartGameScreen.tsx
+++ React-Native/randomNumberGame/screens/StartGameScreen.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, TextInput, View } from "react-native";
 export default function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad" />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -14,10 +14,26 @@ export default function StartGameScreen() {
 
 const styles = StyleSheet.create({
   inputContainer: {
-    flex: 1,
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
     backgroundColor: "#72063c",
+    borderRadius: 8,
+    elevation: 4, // android shadow
+    shadowColor: "black", // ios shadow
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    width: 50,
+    height: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 16,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
