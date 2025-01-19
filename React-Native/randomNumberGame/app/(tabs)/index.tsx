66a1d37563903ddda4bb862154ee4e2c958edc88
AYFG <sek82468246@gmail.com>
2025-01-19 23:37:58 +0900

    expo-linear-gradient로 배경 그라데이션

--- React-Native/randomNumberGame/app/(tabs)/index.tsx
+++ React-Native/randomNumberGame/app/(tabs)/index.tsx
@@ -1,5 +1,17 @@
 import StartGameScreen from "@/screens/StartGameScreen";
+import { StyleSheet, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function HomeScreen() {
-  return <StartGameScreen />;
+  return (
+    <LinearGradient colors={["#ddb52f", "#4e0329"]} style={styles.rootScreen}>
+      <StartGameScreen />
+    </LinearGradient>
+  );
 }
+
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  },
+});
