15d1444d40baea62ee1a28e1b5d0af921876e79b
AYFG <sek82468246@gmail.com>
2025-01-20 23:26:07 +0900

    imageBackground로 이미지 넣기

--- React-Native/randomNumberGame/app/(tabs)/index.tsx
+++ React-Native/randomNumberGame/app/(tabs)/index.tsx
@@ -1,11 +1,18 @@
 import StartGameScreen from "@/screens/StartGameScreen";
-import { StyleSheet, View } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import { StyleSheet, ImageBackground } from "react-native";
 
 export default function HomeScreen() {
   return (
     <LinearGradient colors={["#ddb52f", "#4e0329"]} style={styles.rootScreen}>
-      <StartGameScreen />
+      <ImageBackground
+        source={require("../../assets/images/background.png")}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -13,5 +20,8 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.2,
   },
 });
