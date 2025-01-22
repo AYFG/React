d483752e6ee511c68f567a1845cb52be945cb85c
AYFG <sek82468246@gmail.com>
2025-01-22 23:59:23 +0900

    폴더 구조 변경, 게임 로직 생성

--- React-Native/randomNumberGame/components/ui/PrimaryButton.tsx
+++ React-Native/randomNumberGame/components/ui/PrimaryButton.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Pressable, StyleSheet, Text, View } from "react-native";
-import { Colors } from "../constants/Colors";
+import { Colors } from "../../constants/Colors";
 
 export default function PrimaryButton({
   children,
--- React-Native/randomNumberGame/screens/GameScreen.tsx
+++ React-Native/randomNumberGame/screens/GameScreen.tsx
@@ -1,10 +1,43 @@
-import Title from "@/components/Title";
+import NumberContainer from "@/components/game/NumberContainer";
+import PrimaryButton from "@/components/ui/PrimaryButton";
+import Title from "@/components/ui/Title";
+import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function GameScreen() {
+function generateRandomBetween(min: number, max: number, exclude: number) {
+  const randomNumber = Math.floor(Math.random() * (max - min)) + min;
+
+  if (randomNumber === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return randomNumber;
+  }
+}
+
+let minBoundary = 1;
+let maxBoundary = 100;
+
+export default function GameScreen({ userNumber }: { userNumber: number }) {
+  const initialGuess = generateRandomBetween(minBoundary, maxBoundary, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  function nextGuessHandler(direction) {
+    if (direction === "lower") {
+      maxBoundary = currentGuess - 1;
+    } else {
+      minBoundary = currentGuess + 1;
+    }
+    const newRandomNumber = generateRandomBetween(minBoundary, maxBoundary, currentGuess);
+  }
+
   return (
     <View style={styles.screen}>
       <Title>상대방의 추측</Title>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <View style={styles.buttonContainer}>
+        <PrimaryButton>+</PrimaryButton>
+        <PrimaryButton>-</PrimaryButton>
+      </View>
       <View>
         <Text>Higher or lower?</Text>
       </View>
@@ -17,5 +50,9 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 40,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: "row",
   },
 });
