45e4e0b970f327e77372a45a67475d8273306a7d
AYFG <sek82468246@gmail.com>
2025-02-14 22:08:41 +0900

    gameover 페이지 rotate 반응형 디자인, platform api를 통해 특정 플랫폼용 코드 작성 주석, 상태 바 스타일링

--- React-Native/randomNumberGame/app/(tabs)/index.tsx
+++ React-Native/randomNumberGame/app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import GameScreen from "@/screens/GameScreen";
 import StartGameScreen from "@/screens/StartGameScreen";
 import { LinearGradient } from "expo-linear-gradient";
 import { useState } from "react";
-import { StyleSheet, ImageBackground, SafeAreaView } from "react-native";
+import { StyleSheet, ImageBackground, SafeAreaView, StatusBar } from "react-native";
 import { Colors } from "../../constants/Colors";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
@@ -51,16 +51,19 @@ export default function HomeScreen() {
   }
 
   return (
-    <LinearGradient colors={[Colors.primary700, Colors.accent500]} style={styles.rootScreen}>
-      <ImageBackground
-        source={require("../../assets/images/background.png")}
-        resizeMode="cover"
-        style={styles.rootScreen}
-        imageStyle={styles.backgroundImage}
-      >
-        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
-      </ImageBackground>
-    </LinearGradient>
+    <>
+      <StatusBar barStyle="light-content" />
+      <LinearGradient colors={[Colors.primary700, Colors.accent500]} style={styles.rootScreen}>
+        <ImageBackground
+          source={require("../../assets/images/background.png")}
+          resizeMode="cover"
+          style={styles.rootScreen}
+          imageStyle={styles.backgroundImage}
+        >
+          <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
+        </ImageBackground>
+      </LinearGradient>
+    </>
   );
 }
 
--- React-Native/randomNumberGame/components/ui/Title.tsx
+++ React-Native/randomNumberGame/components/ui/Title.tsx
@@ -1,5 +1,5 @@
 import { Colors } from "@/constants/Colors";
-import { StyleSheet, Text } from "react-native";
+import { Platform, StyleSheet, Text } from "react-native";
 
 export default function Title({ children }: { children: string }) {
   return <Text style={styles.title}>{children}</Text>;
@@ -13,6 +13,9 @@ const styles = StyleSheet.create({
     color: "white",
     textAlign: "center",
     borderWidth: 2,
+    // Platform 별로 다르게 지정하는 방법
+    // borderWidth: Platform.OS === "android" ? 2 : 0,
+    // borderWidth: Platform.select({ ios: 0, android: 2 }),
     borderColor: "white",
     padding: 12,
     maxWidth: "80%",
