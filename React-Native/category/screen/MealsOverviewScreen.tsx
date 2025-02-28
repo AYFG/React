3f622b67b8464799cde28abdaac243473e8e555b
AYFG <sek82468246@gmail.com>
2025-02-28 20:15:24 +0900

    add image

--- React-Native/category/screen/MealsOverviewScreen.tsx
+++ React-Native/category/screen/MealsOverviewScreen.tsx
@@ -1,10 +1,41 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
-export default function MealsOverviewScreen({ route }) {
+import { RouteProp } from "@react-navigation/native";
+import Meal from "../models/meal";
+
+interface RenderMealItemProps {
+  item: Meal;
+}
+
+type MealsOverviewScreenRouteProp = RouteProp<{ params: { categoryId: string } }, "params">;
+
+export default function MealsOverviewScreen({ route }: { route: MealsOverviewScreenRouteProp }) {
   const categoryId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(categoryId) >= 0;
+  });
+
+  function renderMealItem(itemData: RenderMealItemProps) {
+    const item = itemData.item;
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      duration: item.duration,
+      complexity: item.complexity,
+      affordability: item.affordability,
+    };
+    return <MealItem {...mealItemProps} />;
+  }
   return (
     <View style={styles.container}>
-      <Text>MealsOverviewScreen - {categoryId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
